bea7fe2b718b2ed17b534f78bed57ffd88146847
erikarg <erikatrue@gmail.com>
2023-01-17 18:15:52 -0300

    feat: add challenge 3

---
+++ tests/unit/Services/CarService.test.ts
@@ -0,0 +1,85 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+
+describe('Testa todas as funcionalidades do CarService', function () {
+  const service = new CarService();
+  it('Deve registrar um novo carro com sucesso', async function () {
+    const input: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    const output: ICar = {
+      id: '63c70797512bd8e8bcc95222',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    sinon.stub(Model, 'create').resolves(output);
+
+    const result = await service.registration(input);
+
+    expect(result).to.be.deep.equal(output);
+  });
+
+  it('Deve recuperar todos os carros cadastrados no banco de dados', async function () {
+    const output = [
+      {
+        id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      },
+      {
+        id: '634852326b35b59438fbea31',
+        model: 'Tempra',
+        year: 1995,
+        color: 'Black',
+        buyValue: 39,
+        doorsQty: 2,
+        seatsQty: 5,
+      },
+    ];
+    const carMap = output.map((car) => new Car(car));
+    sinon.stub(Model, 'find').resolves(output);
+    const result = await service.getAll();
+    expect(result).to.be.deep.equal(carMap);
+  });
+
+  it('Deve recuperar o carro através do id fornecido', async function () {
+    const output = {
+      id: '63c70797512bd8e8bcc95222',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.99,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+    sinon.stub(Model, 'find').resolves([output]);
+
+    const result = await service.getById(output.id);
+    expect(result).to.be.deep.equal(output);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
